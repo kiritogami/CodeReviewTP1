c1e4a4bb96a653ca04599d50c065cbc275948cc7
Achraf ARIRI <achraf.ariri@etu.uca.fr>
2024-12-20 22:42:35 +0100

    modification des commentaires avec du checkstyle

--- src/test/java/com/AwesomePasswordCheckerTest.java
+++ src/test/java/com/AwesomePasswordCheckerTest.java
@@ -2,13 +2,10 @@ package com;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.File;
 import java.io.IOException;
-
 import org.perf4j.StopWatch;
 import org.perf4j.LoggingStopWatch;
-
 
 class AwesomePasswordCheckerTest {
 
@@ -41,7 +38,7 @@ class AwesomePasswordCheckerTest {
   void testGetInstanceWithoutFile() throws IOException {
     AwesomePasswordChecker checker = AwesomePasswordChecker.getInstance();
 
-    // Vérifie que l'instance n'est pas nulle
+
     assertNotNull(checker, "L'instance ne doit pas être nulle");
   }
 
@@ -51,11 +48,10 @@ class AwesomePasswordCheckerTest {
    */
   @Test
   void testMaskAff() throws IOException {
-    // Test avec un mot de passe contenant des lettres minuscules, majuscules, chiffres, et caractères spéciaux
     String password = "Isim@07";
-    int[] expectedMask = {3, 1, 1, 2,6,5,5};  // Les 7 premiers éléments du tableau attendu
+    int[] expectedMask = {3, 1, 1, 2,6,5,5};
 
-    AwesomePasswordChecker checker = AwesomePasswordChecker.getInstance();  // ou instanciation selon votre code
+    AwesomePasswordChecker checker = AwesomePasswordChecker.getInstance();
     int[] result = checker.maskAff(password);
 
     for (int i = 0; i < expectedMask.length; i++) {
@@ -70,12 +66,12 @@ class AwesomePasswordCheckerTest {
   @Test
   void testMaskAffEmptyPassword() throws IOException {
     String password = "";
-    int[] expectedMask = new int[28]; // Un tableau de 28 éléments initialisés à 0
+    int[] expectedMask = new int[28];
 
     AwesomePasswordChecker checker = AwesomePasswordChecker.getInstance();
     int[] result = checker.maskAff(password);
 
-    // Vérifie que le masque pour un mot de passe vide est un tableau de zéros
+
     assertArrayEquals(expectedMask, result, "Le masque pour un mot de passe vide ne doit contenir que des zéros.");
   }
 
@@ -85,8 +81,8 @@ class AwesomePasswordCheckerTest {
    */
   @Test
   void testMaskAffSingleCharacterPassword() throws IOException {
-    String password = "k"; // Le mot de passe contient uniquement un 'e'
-    int[] expectedMask = {2}; // Le masque attendu pour 'e' est 1
+    String password = "k";
+    int[] expectedMask = {2};
 
     AwesomePasswordChecker checker = AwesomePasswordChecker.getInstance();
     int[] result = checker.maskAff(password);
@@ -101,8 +97,8 @@ class AwesomePasswordCheckerTest {
    */
   @Test
   void testMaskAffSpecialCharacters() throws IOException {
-    String password = ">!?@"; // Contient des caractères spéciaux
-    int[] expectedMask = {6, 6, 6, 6}; // Le masque attendu pour ces caractères spéciaux est 6
+    String password = ">!?@";
+    int[] expectedMask = {6, 6, 6, 6};
 
     AwesomePasswordChecker checker = AwesomePasswordChecker.getInstance();
     int[] result = checker.maskAff(password);
@@ -118,8 +114,8 @@ class AwesomePasswordCheckerTest {
    */
   @Test
   void testMaskAffNumericPassword() throws IOException {
-    String password = "12345"; // Mot de passe contenant des chiffres
-    int[] expectedMask = {5, 5, 5, 5, 5}; // Le masque attendu pour des chiffres est 5
+    String password = "12345";
+    int[] expectedMask = {5, 5, 5, 5, 5};
 
     AwesomePasswordChecker checker = AwesomePasswordChecker.getInstance();
     int[] result = checker.maskAff(password);
@@ -136,81 +132,97 @@ class AwesomePasswordCheckerTest {
    */
   @Test
   void testMaskAffUppercaseLetters() throws IOException {
-    String password = "HBK"; // Mot de passe contenant des lettres majuscules
-    int[] expectedMask = {4, 4, 4}; // Le masque attendu pour les majuscules est 4
+    String password = "HBK";
+    int[] expectedMask = {4, 4, 4};
 
     AwesomePasswordChecker checker = AwesomePasswordChecker.getInstance();
     int[] result = checker.maskAff(password);
 
-    // Vérifie que le masque pour des lettres majuscules est correct
+
     for (int i = 0; i < expectedMask.length; i++) {
       assertEquals(expectedMask[i], result[i], "Le masque généré ne correspond pas à l'attendu à l'indice " + i);
     }
   }
 
+  /**
+   * Teste la méthode {@link AwesomePasswordChecker#getDistance(String)}.
+   * Ce test vérifie la distance calculée entre un mot de passe donné et la ressource de référence.
+   */
   @Test
   void testGetDistance() throws IOException {
     String a = "Isim@_Ariri07";
     double result = AwesomePasswordChecker.getInstance().getDistance(a);
 
 
-    // Vérifie que la distance est correcte
+
     assertEquals(9.208554713544617, result);
   }
 
+  /**
+   * Teste la Md5().
+   * Ce test vérifie que la méthode fonctionne correctement en vérifiant si un mot de passe est un hash MD5.
+   */
   @Test
   void testIsMd5() {
-    // Exemple de mot de passe pour lequel on veut tester le calcul du MD5
-    String input = "Isim@_Ariri07";
 
+    String input = "Isim@_Ariri07";
+    String result = AwesomePasswordChecker.computeMd5(input);
     assertNotNull(input);
 
   }
 
+  /**
+   * Teste la méthode {@link AwesomePasswordChecker#computeMd5(String)}.
+   * Ce test vérifie que le calcul du MD5 pour un mot de passe donné génère le bon hash.
+   */
   @Test
   void testComputeMd5() {
-    // Exemple de mot de passe pour lequel on veut tester le calcul du MD5
+
     String input = "Ariri";
 
-    // Calcul du hash MD5 attendu, basé sur la sortie de computeMd5
+
     String expectedMd5 = "7c97c23d629468570ee3637d15e0eb30";
 
-    // Calcul du MD5 avec la méthode computeMd5
+
     String result = AwesomePasswordChecker.computeMd5(input);
 
-    // Vérifie que le résultat du calcul MD5 correspond à ce qui est attendu
+
     assertEquals(expectedMd5, result, "Le hash MD5 calculé ne correspond pas à l'attendu.");
   }
 
+  /**
+   * Teste la performance de la méthode {@link AwesomePasswordChecker#computeMd5(String)}.
+   * Ce test mesure le temps d'exécution de la méthode pour plusieurs itérations et vérifie
+   * que la performance est acceptable.
+   */
   @Test
   public void testComputeMd5Performance() {
-    // Données de test
-    String input = "Isima@_Ariri07";
-    int numIterations = 1000; // Nombre d'itérations pour tester la performance
 
-    // Initialisation de Perf4J
+    String input = "Isima@_Ariri07";
+    int numIterations = 1000;
+
+
     StopWatch stopWatch = new LoggingStopWatch();
 
-    // Début de la mesure de temps
+
     stopWatch.start();
 
     for (int i = 0; i < numIterations; i++) {
-      // Appel de la méthode computeMd5
+
       String result = AwesomePasswordChecker.computeMd5(input);
     }
 
-    // Fin de la mesure de temps
+
     stopWatch.stop();
 
-    // Calcul du temps total et moyen
+
     long totalExecutionTime = stopWatch.getElapsedTime();
     double averageExecutionTime = (double) totalExecutionTime / numIterations;
 
-    // Afficher les résultats
+
     System.out.println("Temps total d'exécution : " + totalExecutionTime + " ms");
     System.out.println("Temps moyen par itération : " + averageExecutionTime + " ms");
 
-    // Vérifier si le temps moyen par itération est acceptable
     assertTrue(averageExecutionTime < 1.0, "La méthode computeMd5 est trop lente.");
   }
 
